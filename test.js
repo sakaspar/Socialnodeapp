093cfbe66d51e219c8dabb2f3a37cae2b6507ee4
Hamza Saadi <36114417+sakaspar@users.noreply.github.com>
2023-02-05 21:42:24 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const path = require('path');
+// TypeScript: import ytdl from 'ytdl-core'; with --esModuleInterop
+// TypeScript: import * as ytdl from 'ytdl-core'; with --allowSyntheticDefaultImports
+// TypeScript: import ytdl = require('ytdl-core'); with neither of the above
 
+ytdl('https://www.youtube.com/watch?v=YykjpeuMNEk&list=RDuuZE_IRwLNI&index=13')
+  .pipe(fs.createWriteStream('video.mp4'));
+
+
+const directoryPath = path.join(__dirname, '.');
+
+fs.readdir(directoryPath, function (err, files) {
+  if (err) {
+    return console.log('Unable to scan directory: ' + err);
+  }
+  files.forEach(function (file) {
+    console.log(file);
+  });
+});
