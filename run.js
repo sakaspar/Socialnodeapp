7f7e6ed46bdf404e7767b3cdca9a16cc09871126
Hamza Saadi <36114417+sakaspar@users.noreply.github.com>
2023-02-06 17:09:40 +0100

    Update run.js

--- run.js
+++ run.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  ytdl('https://www.youtube.com/watch?v=R1Ehb3JA-cM',filter: 'audioandvideo')
+  ytdl('https://www.youtube.com/watch?v=R1Ehb3JA-cM',{filter: 'audioandvideo'})
   .pipe(fs.createWriteStream('videos/video.mp4'));
 
 
