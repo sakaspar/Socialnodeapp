d3e26172cde612301c79bb32b441b14b2cf8b3cf
Hamza Saadi <36114417+sakaspar@users.noreply.github.com>
2023-02-06 12:30:50 +0100

    Update test.js

--- test.js
+++ test.js
@@ -4,6 +4,10 @@ const path = require('path');
 // TypeScript: import ytdl from 'ytdl-core'; with --esModuleInterop
 // TypeScript: import * as ytdl from 'ytdl-core'; with --allowSyntheticDefaultImports
 // TypeScript: import ytdl = require('ytdl-core'); with neither of the above
+const express = require('express');
+const app = express();
+
+
 
 ytdl('https://www.youtube.com/watch?v=YykjpeuMNEk&list=RDuuZE_IRwLNI&index=13')
   .pipe(fs.createWriteStream('video.mp4'));
@@ -18,4 +22,15 @@ fs.readdir(directoryPath, function (err, files) {
   files.forEach(function (file) {
     console.log(file);
   });
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
