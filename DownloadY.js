e8688ec7255f67a81ce26958c3d1cf6922414bb9
Hamza Saadi <36114417+sakaspar@users.noreply.github.com>
2023-02-06 14:52:23 +0100

    Update DownloadY.js

--- DownloadY.js
+++ DownloadY.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
