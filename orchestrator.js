b15b8f426765350c2a2357c71162e0cb733f4afe
Hamza Saadi <36114417+sakaspar@users.noreply.github.com>
2023-02-09 20:20:50 +0100

    Update orchestrator.js

--- orchestrator.js
+++ orchestrator.js
@@ -2,14 +2,11 @@ const async = require('async');
 const firstScript = require('./generatelinks.js').run; 
 const secondScript = require('./run.js').run;
 const thirdScript = require('./test.js').run;
-async.series([
-  function (callback) {
-    firstScript(callback);
-  },
-  function (callback) {
-    secondScript(callback);
-  },
-  function (callback) {
-    thirdScript(callback);
-  }
-]);
+
+    firstScript();
+
+    secondScript();
+
+    thirdScript();
+
+;
