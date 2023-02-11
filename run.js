bb67072a911ec5fdf067019e9a4d3610036643f4
Hamza Saadi <36114417+sakaspar@users.noreply.github.com>
2023-02-11 14:37:21 +0100

    Update run.js

--- run.js
+++ run.js
@@ -1,13 +1,92 @@
-exports.run = function() {
+//exports.run = async function() {
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
 const ytdl = require('ytdl-core');
+const sanitize = require('sanitize-filename');
 const port = process.env.PORT || 9090;
 const app = express();
 const linksFile = 'links.txt';
+const BATCH_SIZE = 5;
+const WAIT_TIME = 30 * 1000; // 30 seconds
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+
+
+
+//functions
+async function downloadVideos() {
+  try {
+    const data = await fs.promises.readFile(linksFile, 'utf-8');
+    const links = data.split(',');
+
+    const numVideos = links.length;
+    const numBatches = Math.ceil(numVideos / BATCH_SIZE);
+    console.log(`Downloading ${numVideos} videos in ${numBatches} batches...`);
+
+    for (let i = 0; i < numBatches; i++) {
+      console.log(`Batch ${i + 1}/${numBatches}`);
+      const batchStart = i * BATCH_SIZE;
+      const batchEnd = Math.min((i + 1) * BATCH_SIZE, numVideos);
+      const batchLinks = links.slice(batchStart, batchEnd);
+      const promises = [];
+
+      batchLinks.forEach((link) => {
+        const title = sanitize(ytdl.getURLVideoID(link));
+        const videoPath = `videos/${title}.mp4`;
+        const audioPath = `audios/${title}.mp3`;
+
+        console.log(`Downloading ${link}...`);
+        const videoStream = ytdl(link, { filter: 'videoonly' });
+        const videoFile = fs.createWriteStream(videoPath);
+        videoStream.pipe(videoFile);
+        const videoPromise = new Promise((resolve, reject) => {
+          videoFile.on('finish', resolve);
+          videoFile.on('error', reject);
+        });
+
+        const audioStream = ytdl(link, { filter: 'audioonly' });
+        const audioFile = fs.createWriteStream(audioPath);
+        audioStream.pipe(audioFile);
+        const audioPromise = new Promise((resolve, reject) => {
+          audioFile.on('finish', resolve);
+          audioFile.on('error', reject);
+        });
+
+        promises.push(videoPromise, audioPromise);
+      });
+
+      await Promise.all(promises);
+      if (i < numBatches - 1) {
+        console.log(`Pausing until batch ${i + 1} is finished...`);
+        await Promise.all(promises);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//endpoints 
 app.get('/', (req, res) => {
   ytdl('https://www.youtube.com/watch?v=Ul8vqaGGnY0',{filter: 'audioandvideo'})
   .pipe(fs.createWriteStream('videos/video1.mp4'));
@@ -34,36 +113,25 @@ app.get('/new', (req, res) => {
     }
     const links = data.split(',');
     links.forEach((link) => {
+      
       ytdl(link, { filter: 'audioandvideo' })
         .pipe(fs.createWriteStream(`videos/video-${Date.now()}.mp4`));
+      ytdl.getInfo(link,function(err, info) {console.log("info is :",info)})
     });
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
 });
 
-/*
-app.get('/new', (req, res) => {
-  fs.readFile(linksFile, 'utf-8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.sendStatus(500);
-    }
-    const links = data.split(',');
-    links.forEach((link) => {
-      ytdl.getInfo(link, (err, info) => {
-        if (err) {
-          console.error(err);
-          return res.sendStatus(500);
-        }
-        const title = info.title.replace(/\s/g, '-');
-        ytdl(link, { filter: 'audioandvideo' })
-          .pipe(fs.createWriteStream(`videos/${title}.mp4`));
-      });
-    });
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
+
+
+
+app.get('/new1', async (req, res) => {
+ await downloadVideos();
 });
-*/
+
+
+
+
 app.get("/shows", (req, res) => {
   fs.readdir("./videos", (err, files) => {
     if (err) {
@@ -140,81 +208,7 @@ app.get("/shows", (req, res) => {
   });
 });
 
-app.get("/allshows", (req, res) => {
-  fs.readdir("./", (err, files) => {
-    if (err) {
-      console.error(`Error reading directory ./videos.`);
-      console.error(err);
-      res.sendStatus(500);
-      return;
-    }
 
-    const videoFiles = files.filter(
-      (file) => file.endsWith(".mp4"));
-
-
-        let html = `<!doctype html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Videos</title>
-    </head>
-    <body> 
-        <h1>V</h1>
-        
-        <div style="display: flex; flex-wrap: wrap;">`;
-
-        let videoCounter = 0;
-        for (const videoFile of videoFiles) {
-            html += `<div style="flex: 1; padding: 10px;">
-                <div>
-                    <h2>${videoFile}</h2>
-                </div>
-                <video
-                    style="height: 300px"
-                    muted
-                    playsInline
-                    loop
-                    controls
-                    src="/videos/${videoFile}" 
-                    >
-                </video>
-            </div>`;
-            
-            videoCounter++;
-            if (videoCounter % 2 === 0) {
-                html += `</div><div style="display: flex; flex-wrap: wrap;">`;
-            }
-        }
-
-        html += `</div>
-    </body>
-</html>`;
-
-    for (const videoFile of videoFiles) {
-      html += `<div>
-                <div>
-                    <h2>${videoFile}</h2>
-                </div>
-                <video
-                    style="height: 300px"
-                    muted
-                    playsInline
-                    loop
-                    controls
-                    src="/videos/${videoFile}" 
-                    >
-                </video>
-            </div>`;
-    }
-
-    html += `</div>
-    </body>
-</html>`;
-
-    res.send(html);
-  });
-});
 
 
 app.get("/videos/:videoFile", (req, res) => {
@@ -288,4 +282,4 @@ app.listen(port, () => {
   console.log(`Open your browser and navigate to http://localhost:${port}`);
 });
 
-}
+//}
